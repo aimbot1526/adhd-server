f28a56fb3b8dd614f607a9f0a3eb968b75e1b261
aimbot1526 <anshjaamwal2002@gmail.com>
2023-04-21 11:35:15 +0530

    fix: added fiber middleware

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/aimbot1526/adhd-server/cmd/server"
 	"github.com/aimbot1526/adhd-server/configs"
+	"github.com/aimbot1526/adhd-server/pkg/middleware"
 	"github.com/aimbot1526/adhd-server/pkg/routes"
 	"github.com/gofiber/fiber/v2"
 )
@@ -12,6 +13,8 @@ func main() {
 	config := configs.FiberConfig()
 
 	app := fiber.New(config)
+
+	middleware.FiberMiddleware(app)
 
 	routes.ProductRoute(app)
 
--- pkg/middleware/fiber_middleware.go
+++ pkg/middleware/fiber_middleware.go
@@ -8,7 +8,12 @@ import (
 
 func FiberMiddleware(a *fiber.App) {
 	a.Use(
-		cors.New(),
+		cors.New(cors.Config{
+			AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+			AllowOrigins:     "*",
+			AllowCredentials: true,
+			AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		}),
 		logger.New(),
 	)
 }
